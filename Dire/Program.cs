2b14bbbd814c5e1127cc650580ca6990f724ae4e
leafi <leaf@leaf.sx>
2014-05-17 00:56:11 +0100

    strange code

--- Dire/Program.cs
+++ Dire/Program.cs
@@ -1,13 +1,50 @@
-﻿using System;
+﻿using NetMQ;
+using System;
+using System.Collections.Generic;
+
+/* Directory server. Really just a key-value string store.
+ * 
+ * Always hosted on :32770. No ACL yet. TODO: send dontWait option?
+ * 
+ * `? something` looks up 'something', `! one two` sets entry `one` to `two`.
+ */
 
 namespace Dire
 {
   class MainClass
   {
+    static Dictionary<string, string> dir = new Dictionary<string, string>();
+
     public static void Main(string[] args)
     {
-      Console.WriteLine("Hello World!");
-      
+      using (var ctx = NetMQContext.Create())
+      using (var server = ctx.CreateResponseSocket()) {
+        server.Bind("tcp://0.0.0.0:32770");
+
+        while (true) {
+          var parts = server.ReceiveString().Split(new char[] { ' ' }, 3);
+          var ret = "ERR from Dire";
+
+          try {
+
+            if (parts[0] == "?") {
+              ret = dir[parts[1]];
+            } else if (parts[0] == "!") {
+              if (dir.ContainsKey(parts[1]))
+                dir.Remove(parts[1]);
+
+              dir.Add(parts[1], parts[2]);
+              ret = null;
+            }
+
+          } catch (Exception) {
+          } finally {
+            server.Send(ret ?? "");
+          }
+        }
+
+      }
     }
+
   }
 }
---
+++ arsh/Program.cs
@@ -0,0 +1,70 @@
+﻿using NetMQ;
+using System;
+using System.Collections.Generic;
+
+namespace arsh
+{
+  class MainClass
+  {
+    static NetMQContext ctx;
+
+    private static string oneShot(string addr, string msg)
+    {
+      using (var rq = ctx.CreateRequestSocket()) {
+        rq.Options.Linger = TimeSpan.FromSeconds(0.5);
+        rq.Options.SendTimeout = TimeSpan.FromSeconds(0.5);
+
+        Func<Action, string, string> catching = (x, err) => {
+          try {
+            x();
+            return null;
+          } catch (TimeoutException) {
+            return err;
+          }
+        };
+
+        return catching(() => rq.Connect(addr), "ERR connect took too long (anyone home?)")
+          ?? catching(() => rq.Send(msg), "ERR send took too long (busy?)")
+          ?? rq.ReceiveString();
+      }
+    }
+
+    private static string getDire(string key)
+    {
+      return oneShot("tcp://127.0.0.1:32770", "? " + key);
+    }
+
+    private static string setDire(string strong)
+    {
+      return oneShot("tcp://127.0.0.1:32770", "! " + strong);
+    }
+
+    public static void Main(string[] args)
+    {
+      ctx = NetMQContext.Create();
+
+      var d = new Dictionary<string, Func<string, string>>() {
+        { "?", getDire },
+        { "!", setDire }
+      };
+
+      Action<string> w = (l) => Console.WriteLine(l);
+
+      while (true) {
+
+        var s = Console.ReadLine();
+        var parts = s.Split(new char[] { ' ' }, 2);
+
+        try {
+          var r = d[parts[0]](parts[1]);
+          w(r.Length == 0 ? "OK" : r);
+        } catch (IndexOutOfRangeException) {
+          w("ERR from arsh: num args");
+        } catch (KeyNotFoundException) {
+          w("ERR from arsh: not impl");
+        }
+
+      }
+    }
+  }
+}
